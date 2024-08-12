c9e0385b9546233bc0ffd4e5798e3b78fa2e357a
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-08-12 16:01:32 -0600

    Se configuro las cors y archivos estáticos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const{API_VERSION} = require('./constants');
 const app = express();
 
+//Configurar bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//Configurar static files
+app.use(express.static('uploads'));
+
+//Configurar cors
+app.use(cors());
 
 module.exports = app;
