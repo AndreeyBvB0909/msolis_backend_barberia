5e8eb575f3ff3b95799f1da07631353463edb389
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-08-12 15:56:53 -0600

    Body Parser configurado

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const{API_VERSION} = require('./constants');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 module.exports = app;
